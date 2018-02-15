78dfc4240129b5a7851d40c4d5b39f90174374c2
KeiXIII <35160856+KeiXIII@users.noreply.github.com>
2018-02-15 05:11:37 -0800

    Update Event.h

--- Event.h
+++ Event.h
@@ -19,14 +19,13 @@ public:
 	Event(string title, int day, MONTH month, int year, int hour, int minutes, string descript);
 
 	void displayEvent();
+	void displayEvent(ofstream &fout);
 	void editTitle(string newTitle);
 	void editDate(int day, MONTH month, int year, int hour, int minutes);
 	void editDescript(string descript);
 	string getTitle();
 	string getDescription();
 	tm getDate();
-	void editTitle(string title);
-	void editDescription(string description);
-	void editDate(int hour, int minute, int day, int month, int year);
+	
 };
 
