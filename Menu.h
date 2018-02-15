8fb0bd551b4201c15bd5918173ce996750154b08
KeiXIII <35160856+KeiXIII@users.noreply.github.com>
2018-02-15 05:02:55 -0800

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -8,23 +8,26 @@
 #ifndef Menu_h
 #define Menu_h
 
-#include "Event.h"
-#include "HashTable.h"
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <ctime>
+#include <map>
+#include "Event.h"
 
 using namespace std;
 
-class Menu{
+typedef multimap<string, Event> mmap;
+typedef pair<string, Event> mmap_pair;
+
+class Menu
+{
 private:
-    string title,description;
-    int choice, day, hour, minute, year, month;
-    Event e;
+	mmap container;
 public:
-    Menu();
-    ~Menu();
-    void menuStart();
-    bool choice2();
+	Menu(mmap newContainer);
+	~Menu();
+	void start();
 };
 
 #endif /* Menu_h */
